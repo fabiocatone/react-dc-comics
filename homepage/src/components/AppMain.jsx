fa371cb599d66b8e66565897901416b97c4221ee
fabiocatone <fabbiocatone@email.com>
2026-04-20 18:45:34 +0200

    insert img but not ok

--- homepage/src/components/AppMain.jsx
+++ homepage/src/components/AppMain.jsx
@@ -1,8 +1,11 @@
 
-
+import JumbotronImg from "../assets/img/jumbotron.jpg"
 export default function AppMain() {
     
   return  (<main>
+        <div className="jumbotron">
+            <img className="jumboimg" src={JumbotronImg}/>
+        </div>
         <div className="content">
             <h1>
                 Benvenuto nel nostro sito!
